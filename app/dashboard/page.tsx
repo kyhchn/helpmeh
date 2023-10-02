8d8b49584c7927e155c1c8b48bf1d3364319b1fa
kyhchn <alvinkn.dev@gmail.com>
2023-10-03 04:29:59 +0700

    'refactor' define aftersignouturl to root

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,12 +2,20 @@
 import React from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 import { Separator } from "@/components/ui/separator";
 import CreateDialog from "@/components/CreateDialog";
+import { db } from "@/lib/db/index";
+import { $notes } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
 type Props = {};
 
-const Page = (props: Props) => {
+const Page = async (props: Props) => {
+  // const { userId } = auth();
+  // const notes = await db
+  //   .select()
+  //   .from($notes)
+  //   .where(eq($notes.userId, userId!));
   return (
     <div className="bg-gradient-to-r from-yellow-100 to-teal-100 min-h-screen">
       <div className="max-w-[calc(100vw-40px)] mx-auto p-10">
@@ -16,11 +24,16 @@ const Page = (props: Props) => {
             <Button className="bg-teal-500">Back</Button>
           </Link>
           <h1>Notes</h1>
-          <UserButton />
+          <UserButton afterSignOutUrl="/" />
         </div>
         <Separator className="my-3" />
         <div className="grid sm:grid-cols-3 md:grid-cols-5 grid-cols-1 gap-4">
           <CreateDialog />
+          {/* {notes.map((note, index) => (
+            <Link href={"/notes/" + note.id} key={index}>
+              <Button className="bg-teal-500">{note.name}</Button>
+            </Link>
+          ))} */}
         </div>
       </div>
     </div>
