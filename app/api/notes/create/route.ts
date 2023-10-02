ae651bcd630a5eb4edc88bbef678d8647fc35e10
kyhchn <alvinkn.dev@gmail.com>
2023-10-03 04:28:19 +0700

    'feat' provide endpoint to create notebook

---
+++ app/api/notes/create/route.ts
@@ -0,0 +1,35 @@
+import { generateImage, generateImagePrompt } from "@/lib/openai";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { db } from "@/lib/db/index";
+import { $notes } from "@/lib/db/schema";
+export async function POST(req: Request) {
+  const { userId } = auth();
+  if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+  const body = await req.json();
+  const { name } = body;
+  const image_description = await generateImagePrompt(name);
+  if (!image_description)
+    return new NextResponse("Error generating image prompt", { status: 500 });
+
+  const image_url = await generateImage(image_description);
+  if (!image_url)
+    return new NextResponse("Error generating Image", {
+      status: 500,
+    });
+
+  const note_id = await db
+    .insert($notes)
+    .values({
+      name: name,
+      userId: userId,
+      imageUrl: image_url,
+    })
+    .returning({
+      insertedId: $notes.id,
+    });
+  return NextResponse.json({
+    note_id: note_id[0].insertedId,
+  });
+}
