3c251f4c5602acca055eaff054cb0fc6f39ae84f
kyhchn <alvinkn.dev@gmail.com>
2023-10-03 14:25:44 +0700

    'feat' route for save note

---
+++ app/api/notes/save/route.ts
@@ -0,0 +1,38 @@
+import { db } from "@/lib/db";
+import { $notes } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { content, id } = body;
+    console.log(body);
+    if (!content && !id)
+      return new NextResponse("Bad request", { status: 400 });
+
+    const note = (
+      await db.select().from($notes).where(eq($notes.id, id))
+    ).shift();
+
+    if (!note) return new NextResponse("Note not found", { status: 404 });
+
+    if (note.content !== content) {
+      await db.update($notes).set({ content }).where(eq($notes.id, id));
+
+      return NextResponse.json(
+        {
+          message: "Note updated successfully",
+        },
+        { status: 200 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Error updating note",
+      },
+      { status: 500 }
+    );
+  }
+}
