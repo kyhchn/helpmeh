baecb17e478d587b0b3d7ee4481e859864bd4380
kyhchn <alvinkn.dev@gmail.com>
2023-10-03 14:29:09 +0700

    `feat` implement debounce save

--- components/TipTapEditor.tsx
+++ components/TipTapEditor.tsx
@@ -1,11 +1,19 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import TipTapTopbar from "./TipTapTopbar";
 import { Button } from "./ui/button";
-const TipTapEditor = () => {
-  const [content, setContent] = React.useState("");
+import axios from "axios";
+import { NoteType } from "@/lib/db/schema";
+import { useMutation } from "react-query";
+import { Loader2 } from "lucide-react";
+
+type Props = {
+  note: NoteType;
+};
+const TipTapEditor = ({ note }: Props) => {
+  const [content, setContent] = React.useState(note.content);
   const editor = useEditor({
     extensions: [StarterKit],
     autofocus: true,
@@ -14,12 +22,38 @@ const TipTapEditor = () => {
       setContent(editor.getHTML());
     },
   });
+  const saveNote = useMutation({
+    mutationFn: async () => {
+      const response = await axios.post("/api/notes/save", {
+        id: note.id,
+        content: content,
+      });
+    },
+  });
 
+  useEffect(() => {
+    const update = setTimeout(() => {
+      saveNote.mutate(undefined, {
+        onSuccess: () => {
+          console.log("saved");
+        },
+        onError: () => {
+          console.log("error saving");
+        },
+      });
+    }, 500);
+    return () => clearTimeout(update);
+  }, [content]);
   return (
     <>
       <div className="flex justify-between">
         {editor && <TipTapTopbar editor={editor} />}
-        <Button className="bg-teal-500 hover:bg-teal-400">Save</Button>
+        <Button className="bg-teal-500 hover:bg-teal-400" disabled>
+          {saveNote.isLoading && (
+            <Loader2 className="animate-spin mr-1" size={16} />
+          )}
+          Save
+        </Button>
       </div>
 
       <div>
