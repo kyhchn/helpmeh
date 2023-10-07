8c570e6370df36cc7fddf52702188492370c4b95
kyhchn <alvinkn.dev@gmail.com>
2023-10-08 04:08:58 +0700

    `feat` add alert dialog for delete button

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -5,6 +5,17 @@ import { $notes } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
 import { useRouter } from "next/navigation";
 import { Button } from "./ui/button";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 export function DeleteButon({ noteId }: { noteId: number }) {
   const router = useRouter();
@@ -12,13 +23,34 @@ export function DeleteButon({ noteId }: { noteId: number }) {
     try {
       await db.delete($notes).where(eq($notes.id, noteId));
       router.back();
+      router.refresh();
     } catch (error) {
       console.log(error);
     }
   };
   return (
-    <Button className="bg-red-500 hover:bg-red-400" onClick={handleDelete}>
-      Delete
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button className="bg-red-500 hover:bg-red-400">Delete</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete note from
+            my databases.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            className=" bg-red-500 border hover:bg-white hover:border-red-500 hover:text-black"
+            onClick={handleDelete}
+          >
+            Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
